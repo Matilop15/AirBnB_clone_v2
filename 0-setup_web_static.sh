baee8dafb53e74d0c47a406302975982c368b37e
Matilop15 <3959@holbertonschool.com>
2022-04-28 16:43:38 +0000

    fix output

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Bash script that sets up your web servers for the deployment of web_static
+# Script that sets web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/
@@ -16,5 +16,5 @@ printf %s "<html>
 " > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/error_page 404 \/custom_404.html;/ a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
+sudo sed -i "40i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
