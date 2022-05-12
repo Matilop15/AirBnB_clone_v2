f43e4fd6a12d7c739d412b89d1548adfccd659e1
Matilop15 <3959@holbertonschool.com>
2022-05-12 17:28:57 -0300

    add all for task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    - /: Displays 'Hello HBNB!'
+    - /hbnb: Displays 'HBNB'
+    - /c/<text>: Display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    - /python/(<text>): display “Python ”, followed by the value of the text variable 
+    (replace underscore _ symbols with a space ). The default value of text is “is cool”
+    - /number/<n>: display “n is a number” only if n is an integer
+"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_AirBnB():
+    """Displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """ Display C follow by the value of text"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonn(text="is cool"):
+    """Display default= is cool, other case = Python + <text>"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def integ(n):
+    """Display "<n> is a number" only if <n> is a integer"""
+    if type(n) == int:
+        return "{} is a number".format(n)
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_html(n):
+    """Display HTML and change h1 page only if n is an integer"""
+    return render_template("5-number.html", n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
